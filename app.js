495cf8bcb16fa86fad5c85faaef7ab271ab5ef74
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-11-15 16:16:41 +0100

    Links integrated
    Men kan nu ook links meegeven

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var app = angular.module('flapperNews', []);
 app.controller('MainCtrl', [
 '$scope',
 function($scope){
-	$scope.test = 'Hello world';
+	$scope.test = 'Flapper News';
 	$scope.posts = [
 	{title: 'post 1', upvotes: 5},
 	{title: 'post 2', upvotes: 2},
@@ -12,11 +12,12 @@ function($scope){
 	];
 	$scope.addPost = function(){
 		if(!$scope.title || $scope.title === ''){return;}
-		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.posts.push({title: $scope.title, link: $scope.link, upvotes: 0});
 		$scope.title = '';
+		$scope.link = '';
 	};
 	$scope.incrementUpvotes = function(post){
 		post.upvotes += 1;
-	}
+	};
 
 }]);
