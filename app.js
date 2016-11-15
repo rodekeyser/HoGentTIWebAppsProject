18943e74260eae98fc6c2effea2feddb54066e2d
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-11-15 19:19:30 +0100

    Nieuwe state + controller aangemaakt
    voor comments

--- app.js
+++ app.js
@@ -29,12 +29,39 @@ function($scope, posts){
 
 }]);
 
+app.controller('PostsCtrl', [
+'$scope',
+'$stateParams',
+'posts',
+function($scope, $stateParams, posts){
+	
+	$scope.post = posts.posts[$stateParams.id];
+	
+	$scope.posts.push({
+		title: $scope.title,
+		link: $scope.link,
+		upvotes:0,
+		comments: [
+		{author: 'Joe', body: 'Cool post!', upvotes: 0},
+		{authos: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+		]
+	});
+	
+}]);
+
+
 app.config([
 '$stateProvider',
 '$urlRouterProvider',
 function($stateProvider, $urlRouterProvider){
 	$stateProvider.state('home', {
 		url: '/home', templateUrl: '/home.html', controller: 'MainCtrl'
+	});
+	
+	$stateProvider.state('posts', {
+		url: '/posts/{id}',
+		templateUrl: '/posts.html',
+		controller: 'PostsCtrl'
 	});
 	
 	$urlRouterProvider.otherwise('home')
