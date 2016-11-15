6dd3f2b8c8ad6ab5c72aef8fb1ee423d98bf246a
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-11-15 17:45:01 +0100

    UI-router geïnstalleerd

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ui.router']);
 
 app.factory('posts', [function(){
 	var o = {
@@ -29,3 +29,13 @@ function($scope, posts){
 
 }]);
 
+app.config([
+'$stateProvider',
+'$urlRouterProvider',
+function($stateProvider, $urlRouterProvider){
+	$stateProvider.state('home', {
+		url: '/home', templateUrl: '/home.html', controller: 'MainCtrl'
+	});
+	
+	$urlRouterProvider.otherwise('home')
+}]);
