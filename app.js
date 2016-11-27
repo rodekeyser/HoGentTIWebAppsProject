c9864887d16ff69c1479f7aa24aa101c44b160db
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-11-27 16:52:33 +0100

    packages installed in node prompt

--- app.js
+++ app.js
@@ -47,6 +47,16 @@ function($scope, $stateParams, posts){
 		]
 	});
 	
+	$scope.addComment = function(){
+		if($scope.body === ''){return;}
+		$scope.post.comments.push({
+			body: $scope.body,
+			author: 'user',
+			upvotes: 0
+		});
+		$scope.body = '';
+	};
+	
 }]);
 
 
