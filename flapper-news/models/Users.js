093eebfee1ea985a9a0f5c728ee4e1db683db6f6
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-12-04 18:42:03 +0100

    bugfixes + last adjustments to the view for the authentication

--- flapper-news/models/Users.js
+++ flapper-news/models/Users.js
@@ -8,17 +8,22 @@ var UserSchema = new mongoose.Schema({
   salt: String
 });
 
-UserSchema.methods.setPassword = function(password){
+UserSchema.methods.setPassword = function(password)
+{
   this.salt = crypto.randomBytes(16).toString('hex');
-  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
+
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000,64).toString('hex');
 };
 
-UserSchema.methods.validPassword = function(password) {
+UserSchema.methods.validPassword = function(password)
+{
   var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64).toString('hex');
 
   return this.hash === hash;
-};
-UserSchema.methods.generateJWT = function() {
+}
+
+UserSchema.methods.generateJWT = function()
+{
   var today = new Date();
   var exp = new Date(today);
   exp.setDate(today.getDate() + 60);
@@ -26,7 +31,7 @@ UserSchema.methods.generateJWT = function() {
   return jwt.sign({
     _id: this._id,
     username: this.username,
-    exp: parseInt(exp.getTime() / 1000),
+    exp: parseInt(exp.getTime() / 1000)
   }, 'SECRET');
 };
 
