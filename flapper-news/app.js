7c4a7568c326f40fe474d9a8dd783b51bc84b9ef
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-12-04 16:14:11 +0100

    added passport

--- flapper-news/app.js
+++ flapper-news/app.js
@@ -5,13 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-
-require('./models/Posts');
-require('./models/Comments');
-
-mongoose.connect('mongodb://localhost/news');
+var passport = require('passport');
 var index = require('./routes/index');
 var users = require('./routes/users');
+require('./models/Posts');
+require('./models/Comments');
+require('./models/Users');
+require('./config/passport');
+mongoose.connect('mongodb://localhost/news');
 
 
 
@@ -28,7 +29,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
 app.use('/', index);
 app.use('/users', users);
 
