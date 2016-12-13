376fc015ecf7057bf268819eb04a1d5b1596936c
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-12-13 18:11:06 +0100

    change

--- angularjs-realworld-example-app/src/js/components/index.js
+++
@@ -1,8 +0,0 @@
-import angular from 'angular';
-
-let componentsModule = angular.module('app.components', []);
-
-import ListErrors from './list-errors.component';
-componentsModule.component('ListErrors', ListErrors);
-
-export default componentsModule;
