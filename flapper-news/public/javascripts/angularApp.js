b766bd85aac42d74d5c6592f4c2b6f894218e005
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-12-22 22:22:12 +0100

    sdfd

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -153,39 +153,6 @@ app.controller('PostsCtrl',[
        };
   }]);
 
-  app.controller('AuthCtrl', [
-    '$scope',
-    '$state',
-    'auth',
-    function($scope, $state, auth){
-      $scope.user = {};
-
-      $scope.register = function(){
-        auth.register($scope.user).error(function(error){
-          $scope.error = error;
-        }).then(function(){
-          $state.go('home');
-        });
-    };
-
-    $scope.logIn = function(){
-      auth.logIn($scope.user).error(function(error){
-        $scope.error = error;
-      }).then(function(){
-        $state.go('home');
-      });
-    };
-  }]);
-
-  app.controller('NavCtrl', [
-    '$scope',
-    'auth',
-    function($scope, auth){
-      $scope.isLoggedIn = auth.isLoggedIn();
-      $scope.currentUser = auth.currentUser;
-      $scope.logOut = auth.logOut;
-    }
-  ]);
 
 app.controller('AuthCtrl', [
   '$scope',
