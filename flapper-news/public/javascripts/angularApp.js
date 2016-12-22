990d6723b81e550bbf1f94906983d449fe22eccc
Robin De Keyser <robin.dekeyser.v5909@student.hogent.be>
2016-12-22 22:04:25 +0100

    directive

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -125,7 +125,6 @@ app.controller('MainCtrl', [
       posts.upvote(post);
     };
 }]);
-
 app.controller('PostsCtrl',[
   '$scope',
   'posts',
@@ -151,7 +150,13 @@ app.controller('PostsCtrl',[
        $scope.incrementUpvotes = function(comment){
          posts.upvoteComment(post, comment);
        };
-  }]);
+  }])
+  .directive('myPost', function() {
+    return {
+     restrict: 'E',
+     templateUrl: 'my-post.ejs'
+    };
+  });
 
   app.controller('AuthCtrl', [
     '$scope',
@@ -186,39 +191,6 @@ app.controller('PostsCtrl',[
       $scope.logOut = auth.logOut;
     }
   ]);
-
-app.controller('AuthCtrl', [
-  '$scope',
-  '$state',
-  'auth',
-  function($scope, $state, auth){
-    $scope.user = {};
-
-    $scope.register = function(){
-      auth.register($scope.user).error(function(error){
-        $scope.error = error;
-      }).then(function(){
-        $state.go('home');
-      });
-    };
-
-    $scope.logIn = function(){
-      auth.logIn($scope.user).error(function(error){
-        $scope.error = error;
-      }).then(function(){
-        $state.go('home');
-      });
-    };
-  }]);
-
-app.controller('NavCtrl', [
-  '$scope',
-  'auth',
-  function($scope, auth){
-    $scope.isLoggedIn = auth.isLoggedIn;
-    $scope.currentUser = auth.currentUser;
-    $scope.logOut = auth.logOut;
-  }]);
 
   app.config([
   '$stateProvider',
